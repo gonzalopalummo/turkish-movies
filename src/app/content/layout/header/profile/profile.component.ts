2b51c2c1884f72baf223bba4994d798ca7378788
unknown <gonzalo@tixcode.com.ar>
2020-03-11 18:19:25 -0300

    Cleaning app

--- src/app/content/layout/header/profile/profile.component.ts
+++ src/app/content/layout/header/profile/profile.component.ts
@@ -1,41 +1,43 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core'
 
-import { MenuConfigService } from '../../../../core/services/menu-config.service';
-import { SearchService } from '../../../../core/services/search.service';
-import { LoginComponent } from '../login/login.component';
-import {SimpleModalService} from 'ngx-simple-modal';
+import { MenuConfigService } from '../../../../core/services/menu-config.service'
+import { SearchService } from '../../../../core/services/search.service'
+import { LoginComponent } from '../login/login.component'
+import { SimpleModalService } from 'ngx-simple-modal'
 
 @Component({
-    selector: 'app-profile',
-    templateUrl: './profile.component.html'
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
 })
 export class ProfileComponent implements OnInit {
+  @Input() user: any = {}
 
-    @Input() user: any = {};
+  userMenu: any = []
 
-    userMenu: any = [];
+  constructor(
+    private searchService: SearchService,
+    private menuConfigService: MenuConfigService,
+    private simpleModalService: SimpleModalService
+  ) {
+    this.userMenu = this.menuConfigService.userMenuItems
+  }
 
-    constructor(private searchService: SearchService,
-                private menuConfigService: MenuConfigService,
-                private simpleModalService: SimpleModalService) {
-        this.userMenu = this.menuConfigService.userMenuItems;
-    }
+  ngOnInit() {}
 
-    ngOnInit() {
-    }
+  closeSearchResult() {
+    this.searchService.hideSearchResult()
+  }
 
-    closeSearchResult() {
-        this.searchService.hideSearchResult();
-    }
-
-    openLoginModal() {
-        this.closeSearchResult();
-        const modal = this.simpleModalService.addModal(LoginComponent, {})
-        .subscribe((isConfirmed) => {
-            if (isConfirmed) {
-            } else {
-            }
-        });
-    }
-
+  openLoginModal() {
+    this.closeSearchResult()
+    const modal = this.simpleModalService
+      .addModal(LoginComponent, {})
+      .subscribe(isConfirmed => {
+        if (isConfirmed) {
+          console.log('Confirmed')
+        } else {
+          console.log('Not Confirmed')
+        }
+      })
+  }
 }
--- src/app/content/pages/pages-routing.module.ts
+++ src/app/content/pages/pages-routing.module.ts
@@ -1,27 +1,31 @@
-import { NgModule } from "@angular/core";
-import { Routes, RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core'
+import { Routes, RouterModule } from '@angular/router'
 
-import { DemoPageComponent } from "./snippets/demo-page/demo-page.component";
-import { LandingPageComponent } from "./snippets/landing-page/landing-page.component";
-import { ErrorPageComponent } from "./snippets/error-page/error-page.component";
-import { PagesComponent } from "./pages.component";
-import { HomeComponent } from "./components/home/home.component";
+import { DemoPageComponent } from './snippets/demo-page/demo-page.component'
+import { LandingPageComponent } from './snippets/landing-page/landing-page.component'
+import { ErrorPageComponent } from './snippets/error-page/error-page.component'
+import { PagesComponent } from './pages.component'
+import { HomeComponent } from './components/home/home.component'
 
 const routes: Routes = [
   {
-    path: "",
+    path: '',
     component: PagesComponent,
     children: [
       {
-        path: "",
-        loadChildren: "./components/components.module#ComponentsModule"
-      }
-    ]
-  }
-];
+        path: '',
+        loadChildren: './components/components.module#ComponentsModule',
+      },
+    ],
+  },
+  {
+    path: 'landing',
+    component: LandingPageComponent,
+  },
+]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class PagesRoutingModule {}
